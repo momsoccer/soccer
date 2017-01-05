d171dd31329f07787c395257e694418bf5675cea
momsoccer <ishimsungbo@gmail.com>
2017-01-05 12:30:51 +0900

    Initial Commit

---
+++ soccer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soccer
+//
+//  Created by 심성보 on 2017. 1. 5..
+//  Copyright © 2017년 심성보. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
